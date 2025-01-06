17ac251e13fb94e40117725ccf5a9edaad4dad70
Samuk <marcelolamas19@gmail.com>
2025-01-05 22:04:08 -0300

    Refined validation messages; added CSS for message ellipsis

--- src/validators/messageValidator.ts
+++ src/validators/messageValidator.ts
@@ -4,11 +4,15 @@ export const validatorMessageForm = [
 	body("title")
 		.trim()
 		.escape()
-		.isLength({ min: 2, max: 50 })
-		.withMessage("Title must be at least 2 characters long"),
+		.isLength({ min: 2 })
+		.withMessage("Title must be at least 2 characters long.")
+		.isLength({ max: 50 })
+		.withMessage("Title must be at most 50 characters long."),
 	body("message")
 		.trim()
 		.escape()
-		.isLength({ min: 2, max: 600 })
-		.withMessage("Message must be at least 2 characters long"),
+		.isLength({ min: 2 })
+		.withMessage("First name must be at least 2 characters long.")
+		.isLength({ max: 600 })
+		.withMessage("First name must be at most 600 characters long."),
 ];
--- src/validators/signUpValidator.ts
+++ src/validators/signUpValidator.ts
@@ -5,21 +5,29 @@ export const validatorSignUpForm = [
 		.trim()
 		.escape()
 		.isLength({ min: 2 })
-		.withMessage("First name must be at least 2 characters long"),
+		.withMessage("First name must be at least 2 characters long.")
+		.isLength({ max: 50 })
+		.withMessage("First name must be at most 50 characters long."),
 	body("lastName")
 		.trim()
 		.escape()
 		.isLength({ min: 2 })
-		.withMessage("Last name must be at least 2 characters long"),
+		.withMessage("First name must be at least 2 characters long.")
+		.isLength({ max: 50 })
+		.withMessage("First name must be at most 50 characters long."),
 	body("username")
 		.trim()
 		.escape()
 		.isLength({ min: 2 })
-		.withMessage("Username must be at least 2 characters long"),
+		.withMessage("Username must be at least 2 characters long.")
+		.isLength({ max: 15 })
+		.withMessage("Username must be at most 15 characters long."),
 	body("password")
 		.trim()
 		.isLength({ min: 5 })
-		.withMessage("Password must be at least 5 characters long"),
+		.withMessage("Password must be at least 5 characters long.")
+		.isLength({ max: 40 })
+		.withMessage("Password must be at most 40 characters long."),
 	body("confirmPassword").custom((value, { req }) => {
 		if (value !== req.body.password) {
 			console.log(value, req.body.password);
